75085547c19fd0a12b6af0d1e0348d839c0022a0
linkangjie <linkangjie9726@163.com>
2025-04-23 15:56:35 +0800

    commit

--- inkeep.js
+++ inkeep.js
@@ -1,25 +1,32 @@
-import algoliasearch from 'algoliasearch/lite';
-import instantsearch from 'instantsearch.js';
-import { searchBox, hits } from 'instantsearch.js/es/widgets';
+// algolia-search.js
 
-const searchClient = algoliasearch('03J76WSHJY', 'f3ba89d09f20abfdfde09b0bd53102fa');
+(function () {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdn.jsdelivr.net/npm/instantsearch.js@4.0.0/dist/instantsearch.production.min.js";
+  script.onload = function () {
+    const search = instantsearch({
+      indexName: "12-f68f320d",
+      searchClient: algoliasearch(
+        "03J76WSHJY",
+        "f3ba89d09f20abfdfde09b0bd53102fa"
+      ),
+    });
 
-const search = instantsearch({
-  indexName: '12-f68f320d',
-  searchClient,
-});
+    search.addWidgets([
+      instantsearch.widgets.searchBox({
+        container: "#search-box",
+        placeholder: "搜索文档...",
+      }),
+      instantsearch.widgets.hits({
+        container: "#hits",
+        templates: {
+          item: '<div>{{#helpers.highlight}}{ "attribute": "title" }}{{/helpers.highlight}}</div>',
+        },
+      }),
+    ]);
 
-search.addWidgets([
-  searchBox({
-    container: '#search-box',
-    placeholder: '搜索文档...',
-  }),
-  hits({
-    container: '#hits',
-    templates: {
-      item: '<div>{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</div>',
-    },
-  }),
-]);
-
-search.start();
+    search.start();
+  };
+  document.head.appendChild(script);
+})();
